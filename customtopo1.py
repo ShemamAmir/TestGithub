0c05f8257e40ea7e67522c5230f05b541af152dd
ShemamAmir <shamamamer@yahoo.com>
2016-01-27 11:56:30 +0300

    Create customtopo1.py

---
+++ customtopo1.py
@@ -0,0 +1,6 @@
+from mininet.cli import CLI
+from mininet.util import dumpNodeConnections
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+class singleTopologyPerformance(Topo):
+def__init__(self,k=3)
