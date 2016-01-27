38b3818248c079a5601b14b9ee4a94ea5e13d606
ShemamAmir <shamamamer@yahoo.com>
2016-01-27 13:06:31 +0300

    Update customtopo1.py

--- customtopo1.py
+++ customtopo1.py
@@ -4,3 +4,27 @@ from mininet.node import CPULimitedHost
 from mininet.link import TCLink
 class singleTopologyPerformance(Topo):
 def__init__(self,k=3)
+  Topo.__init__(self)
+  switch=self.addSwitch('switch1')
+  linkoptions=dict(bw=10,delay='10ms',max_queue_size=1000,use_htb=true)
+  for h in range(k):
+      host=self.addHost('h%s'%(h+1),cpu=.4/k)
+      self.addLink(host,switch,**linloptions)
+      def perfprmanceTest():
+        topo=SingleTopologyPerformance(k=5)
+        net=Mininet(topo=topo,host=CPULimitedHost,link=TCLink)
+        net.start()
+        print "displaying host connection information"
+        dumpNodeConnections(net.hosts)
+        print "testing network connectivity"
+        net-pingAll()
+        print "checking BW between h1 and h3"
+        h1,h3=net.get('h1','h3')
+        net.iperf((h1,h3))
+        net.stop()
+        if__name__=='__main__'
+        setLogLevel('info')
+        performanceTest()
+        
+    
+  
