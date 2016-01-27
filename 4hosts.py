12e945b2c5fe6321df9d8083e5da7c7c7cfe4512
ShemamAmir <shamamamer@yahoo.com>
2016-01-27 23:14:14 +0300

    Update 4hosts.py

--- 4hosts.py
+++ 4hosts.py
@@ -7,15 +7,15 @@ class SingleSwitchTopo(Topo):
  def __init__(self,n=2,**opts):
  #initialize topo
    Topo.__init__(self, **opts)
-   switch =self.addSwitch('s1')
+   switch = self.addSwitch('s1')
  #range 
    for h in range(n):
      host = self.addHost('h%s'(h+1))
-     self.addLink(host,switch)
+     self.addLink(host, switch)
  def simpleTest():
  #"create and test simple network"
-   mytopo = SingleSwitchTopo(n=4)
-   net = Mininet(mytopo)
+   topo = SingleSwitchTopo(n=4)
+   net = Mininet(topo)
    net.start()
    print "dumping host connection"
    net.pingAll()
