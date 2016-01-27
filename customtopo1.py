7736c962e4964a4ef2f8138bf679b42b47c087af
ShemamAmir <shamamamer@yahoo.com>
2016-01-27 14:41:44 +0300

    Update customtopo1.py

--- customtopo1.py
+++ customtopo1.py
@@ -3,14 +3,14 @@ from mininet.util import dumpNodeConnections
 from mininet.node import CPULimitedHost
 from mininet.link import TCLink
 class singleTopologyPerformance(Topo):
-def __init__(self, k=3):
+          def __init__(self, k=3):
   Topo.__init__(self)
   switch=self.addSwitch('switch1')
   linkoptions=dict(bw=10,delay='10ms',max_queue_size=1000,use_htb=true)
   for h in range(k):
-      host=self.addHost('h%s'%(h+1),cpu=.4/k)
-      self.addLink(host,switch, **linloptions)
-      def perfprmanceTest():
+            host=self.addHost('h%s'%(h+1),cpu=.4/k)
+            self.addLink(host,switch, **linloptions)
+ def perfprmanceTest():
         topo=SingleTopologyPerformance(k=5)
         net=Mininet(topo=topo,host=CPULimitedHost,link=TCLink)
         net.start()
@@ -22,7 +22,7 @@ def __init__(self, k=3):
         h1,h3=net.get('h1','h3')
         net.iperf((h1,h3))
         net.stop()
-        if__name__=='__main__'
+if__name__=='__main__'
         setLogLevel('info')
         performanceTest()
         
